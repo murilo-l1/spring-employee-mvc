88d93835eb370f2e82ed225818c890d0e1a0a813
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-08-08 18:05:44 -0300

    feat: Created formList from Employees retrieved and added register form for POST

---
+++ src/main/java/com/example/employeemvc/exception/InvalidEmployeeCreationException.java
@@ -0,0 +1,7 @@
+package com.example.employeemvc.exception;
+
+public class InvalidEmployeeCreationException extends Exception{
+    public InvalidEmployeeCreationException(String message) {
+        super(message);
+    }
+}
